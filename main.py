5043dafaac5809574d58d6fca559bca0413e4b91
S0m3B0dYyy <108902105+S0m3B0dYyy@users.noreply.github.com>
2023-01-06 22:12:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
 
 def parse(url):
-    if not 't.me/' in url:
+    url = url.replace('\n', '').replace('https://', '').replace('http://', '')
+    url = re.search('t.me\/[a-zA-Z0-9\_]*', url)
+
+    if not url:
         pass
+
     else:
-        url = url.replace('\n', '')
-        url = 'https://' + url if not (
-                url.startswith('https://') or url.startswith('http://')) else url
+        url = url.group(0).strip()
+        url = 'https://' + url
         response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         title = soup.find(
             'div',
-            class_='tgme_page_title').get_text(strip=True)
+            class_='tgme_page_title')
         if title:
             return url
     return None
 
 
 def main():
-    with open(f'links.txt', 'r') as file:
+    with open(f'links.txt', 'r', encoding="utf8") as file:
         links = file.readlines()
     counter, valid = 0, 0
     for link in links:
@@ -30,7 +35,7 @@ def main():
         if res:
             with open(f'result.txt', 'a') as result:
                 valid += 1
-                result.write(link)
+                result.write(f'{res}\n')
                 print(f'{valid}/{counter}/{len(links)} | {res}')
 
 
